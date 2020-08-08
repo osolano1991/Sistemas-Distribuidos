663766624198596870e220358051d80809e56eaf
osolano1991 <oscarsolanomora11@gmail.com>
2020-08-07 21:20:46 -0600

    first commit

---
+++ handlers.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "path"
+)
+
+func find(x string) int {
+    for i, book := range books {
+        if x == book.Id {
+            return i
+        }
+    }
+    return -1
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+    id := path.Base(r.URL.Path)
+    checkError("Parse error", err)
+    i := find(id)
+    if i == -1 {
+        return
+    }
+    dataJson, err := json.Marshal(books[i])
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(dataJson)
+    return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+    len := r.ContentLength
+    body := make([]byte, len)
+    r.Body.Read(body)
+    book := Book{}
+    json.Unmarshal(body, &book)
+    books = append(books, book)
+    w.WriteHeader(200)
+    return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
+    w.WriteHeader(200)
+    return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+    w.WriteHeader(200)
+    return
+}
