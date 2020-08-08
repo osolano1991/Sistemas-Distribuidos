9251fccb3e7984648383473e791c1f65fc5f2073
osolano1991 <oscarsolanomora11@gmail.com>
2020-08-07 21:00:48 -0600

    first commit

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 
 func handler(writer http.ResponseWriter, request *http.Request) {
     fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+    var a = "initial"
+    fmt.Println(a)
 }
 
 func main() {
     http.HandleFunc("/", handler)
     http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
-    var a = "initial"
-    fmt.Println(a)
+    
 }
