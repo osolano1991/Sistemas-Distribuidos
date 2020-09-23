34dae26164cf4683bd33a87315a68bd3e3474030
osolano1991 <oscarsolanomora11@gmail.com>
2020-09-22 22:15:39 -0600

    Microservicios con GoKit

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/osolano1991/Sistemas-Distribuidos
 
 go 1.12
 
-require github.com/gorilla/mux v1.8.0
+require github.com/go-kit/kit v0.10.0 // indirect
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "github.com/go-kit/kit/log"
+    httptransport "github.com/go-kit/kit/transport/http"
+    "github.com/gorilla/mux"
+    "net/http"
+    "os"
+)
+
+func main() {
+    logger := log.NewLogfmtLogger(os.Stderr)
+
+    r := mux.NewRouter()
+
+    var svc BookService
+    svc = NewService(logger)
+
+    // svc = loggingMiddleware{logger, svc}
+    // svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
+
+    CreateBookHandler := httptransport.NewServer(
+        makeCreateBookEndpoint(svc),
+        decodeCreateBookRequest,
+        encodeResponse,
+    )
+    GetByBookIdHandler := httptransport.NewServer(
+        makeGetBookByIdEndpoint(svc),
+        decodeGetBookByIdRequest,
+        encodeResponse,
+    )
+    DeleteBookHandler := httptransport.NewServer(
+        makeDeleteBookEndpoint(svc),
+        decodeDeleteBookRequest,
+        encodeResponse,
+    )
+    UpdateBookHandler := httptransport.NewServer(
+        makeUpdateBookendpoint(svc),
+        decodeUpdateBookRequest,
+        encodeResponse,
+    )
+    http.Handle("/", r)
+    http.Handle("/book", CreateBookHandler)
+    http.Handle("/book/update", UpdateBookHandler)
+    r.Handle("/book/{bookid}", GetByBookIdHandler).Methods("GET")
+    r.Handle("/book/{bookid}", DeleteBookHandler).Methods("DELETE")
+
+    // http.Handle("/metrics", promhttp.Handler())
+    logger.Log("msg", "HTTP", "addr", ":"+os.Getenv("PORT"))
+    logger.Log("err", http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
