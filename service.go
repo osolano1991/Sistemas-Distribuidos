04f43aa41466ea1b89567a7c5d3eb25aef7e54c0
osolano1991 <oscarsolanomora11@gmail.com>
2020-09-26 18:13:17 -0600

    Update service.go

--- service.go
+++ service.go
@@ -92,3 +92,89 @@ func (s bookservice) UpdateBook(ctx context.Context, book Book) (string, error) 
     books[i] = book
     return msg, nil
 }
+//==========================================================================================================================================================
+//                                                                          AUTHORS
+//==========================================================================================================================================================
+type Author struct {
+    AuthorId string `json:"authorId,omitempty"`
+	BookId string `json:"bookId,omitempty"`
+	Name string `json:"name,omitempty"`
+	Nationality string `json:"nationality,omitempty"`
+	Birth string `json:"birth,omitempty"`
+	Genere string `json:"genere,omitempty"`
+}
+
+type authorservice struct {
+    logger log.Logger
+}
+
+// Service describes the Author service.
+type AuthorService interface {
+    CreateAuthor(ctx context.Context, author Author) (string, error)
+    GetAuthorById(ctx context.Context, id string) (interface{}, error)
+    UpdateAuthor(ctx context.Context, author Author) (string, error)
+    DeleteAuthor(ctx context.Context, id string) (string, error)
+}
+
+var authors = []Author{
+    Author{AuthorId: "1", BookId: "1", Name: "Author 1",
+        Nationality: "Costa Rica", Birth: "julio 1991", Genere: "Masculino"},
+    Author{AuthorId: "2", BookId: "2", Name: "Author 2",
+        Nationality: "Espanol", Birth: "agosto 2000", Genere: "Masculino"},
+}
+
+func find(x string) int {
+    for i, author := range authors {
+        if x == author.AuthorId {
+            return i
+        }
+    }
+    return -1
+}
+
+func NewService(logger log.Logger) AuthorService {
+    return &authorservice{
+        logger: logger,
+    }
+}
+
+func (s authorservice) CreateAuthor(ctx context.Context, author Author) (string, error) {
+    var msg = "success"
+    authors = append(authors, author)
+    return msg, nil
+}
+
+func (s authorservice) GetAuthorById(ctx context.Context, id string) (interface{}, error) {
+    var err error
+    var author interface{}
+    var empty interface{}
+    i := find(id)
+    if i == -1 {
+        return empty, err
+    }
+    author = authors[i]
+    return author, nil
+}
+func (s authorservice) DeleteAuthor(ctx context.Context, id string) (string, error) {
+    var err error
+    msg := ""
+    i := find(id)
+    if i == -1 {
+        return "", err
+    }
+    copy(authors[i:], authors[i+1:])
+    authors[len(authors)-1] = Author{}
+    authors = authors[:len(authors)-1]
+    return msg, nil
+}
+func (s bookservice) UpdateAuthor(ctx context.Context, author Author) (string, error) {
+    var empty = ""
+    var err error
+    var msg = "success"
+    i := find(book.AuthorId)
+    if i == -1 {
+        return empty, err
+    }
+    authors[i] = author
+    return msg, nil
+}
