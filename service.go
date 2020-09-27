fb557a011cd2d99b7b249d3c8cc0808119cae3c7
osolano1991 <oscarsolanomora11@gmail.com>
2020-09-26 22:37:08 -0600

    aa

--- service.go
+++ service.go
@@ -178,3 +178,91 @@ func (s authorservice) UpdateAuthor(ctx context.Context, author Author) (string,
     authors[i] = author
     return msg, nil
 }
+
+//==========================================================================================================================================================
+//                                                                          PUBLISHERS
+//==========================================================================================================================================================
+type Publisher struct {
+    PublisherId string `json:"publisherId,omitempty"`
+	Name string `json:"name,omitempty"`
+	Country string `json:"country,omitempty"`
+	Founded string `json:"founded,omitempty"`
+	Genere string `json:"genere,omitempty"`
+}
+
+type publisherservice struct {
+    logger log.Logger
+}
+
+// Service describes the Publisher service.
+type PublisherService interface {
+    CreatePublisher(ctx context.Context, publisher Publisher) (string, error)
+    GetPublisherById(ctx context.Context, id string) (interface{}, error)
+    UpdatePublisher(ctx context.Context, publisher Publisher) (string, error)
+    DeletePublisher(ctx context.Context, id string) (string, error)
+}
+
+var publishers = []Publisher{
+    Publisher{PublisherId: "1", Name: "Yensie", Country: "Inglaterra", Founded: "Costa Rica",
+		Genere: "First"},
+	Publisher{PublisherId: "2", Name: "Tatiana", Country: "Italia", Founded: "Costa Rica",
+		Genere: "Second"},
+	Publisher{PublisherId: "3", Name: "ANGIE", Country: "CHINA", Founded: "CHINA",
+		Genere: "Third"},
+}
+
+func findPublisher(x string) int {
+    for i, publisher := range publishers {
+        if x == publisher.PublisherId {
+            return i
+        }
+    }
+    return -1
+}
+
+func NewServicePublisher(logger log.Logger) PublisherService {
+    return &publisherservice{
+        logger: logger,
+    }
+}
+
+func (s publisherservice) CreatePublisher(ctx context.Context, publisher Publisher) (string, error) {
+    var msg = "success"
+    publishers = append(publishers, publisher)
+    return msg, nil
+}
+
+func (s publisherservice) GetPublisherById(ctx context.Context, id string) (interface{}, error) {
+    var err error
+    var publisher interface{}
+    var empty interface{}
+    i := findPublisher(id)
+    if i == -1 {
+        return empty, err
+    }
+    publisher = publishers[i]
+    return publisher, nil
+}
+func (s publisherservice) DeletePublisher(ctx context.Context, id string) (string, error) {
+    var err error
+    msg := ""
+    i := findPublisher(id)
+    if i == -1 {
+        return "", err
+    }
+    copy(publishers[i:], publishers[i+1:])
+    publishers[len(publishers)-1] = Publisher{}
+    publishers = publishers[:len(publishers)-1]
+    return msg, nil
+}
+func (s publisherservice) UpdatePublisher(ctx context.Context, publisher Publisher) (string, error) {
+    var empty = ""
+    var err error
+    var msg = "success"
+    i := findPublisher(publisher.PublisherId)
+    if i == -1 {
+        return empty, err
+    }
+    publishers[i] = publisher
+    return msg, nil
+}
--- transport.go
+++ transport.go
@@ -232,3 +232,117 @@ type (
         Err error  `json:"error,omitempty"`
     }
 )
+
+//=====================================================================================================================
+//                                                       PUBLISHER
+//=====================================================================================================================
+
+func makeCreatePublisherEndpoint(s PublisherService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(CreatePublisherRequest)
+        msg, err := s.CreatePublisher(ctx, req.publisher)
+        return CreatePublisherResponse{Msg: msg, Err: err}, nil
+    }
+}
+func makeGetPublisherByIdEndpoint(s PublisherService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(GetPublisherByIdRequest)
+        publisherDetails, err := s.GetPublisherById(ctx, req.Id)
+        if err != nil {
+            return GetPublisherByIdResponse{Publisher: publisherDetails, Err: "Id not found"}, nil
+        }
+        return GetPublisherByIdResponse{Publisher: publisherDetails, Err: ""}, nil
+    }
+}
+func makeDeletePublisherEndpoint(s PublisherService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(DeletePublisherRequest)
+        msg, err := s.DeletePublisher(ctx, req.Publisherid)
+        if err != nil {
+            return DeletePublisherResponse{Msg: msg, Err: err}, nil
+        }
+        return DeletePublisherResponse{Msg: msg, Err: nil}, nil
+    }
+}
+func makeUpdatePublisherendpoint(s PublisherService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(UpdatePublisherRequest)
+        msg, err := s.UpdateUpdatePublisher(ctx, req.publisher)
+        return msg, err
+    }
+}
+
+func decodeCreatePublisherRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    var req CreatePublisherRequest
+    fmt.Println("-------->>>>into Decoding")
+    if err := json.NewDecoder(r.Body).Decode(&req.publisher); err != nil {
+        return nil, err
+    }
+    return req, nil
+}
+
+func decodeGetPublisherByIdRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    var req GetPublisherByIdRequest
+    fmt.Println("-------->>>>into GetById Decoding")
+    vars := mux.Vars(r)
+    req = GetPublisherByIdRequest{
+        Id: vars["publisherid"],
+    }
+    return req, nil
+}
+func decodeDeletePublisherRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    fmt.Println("-------->>>> Into Delete Decoding")
+    var req DeletePublisherRequest
+    vars := mux.Vars(r)
+    req = DeletePublisherRequest{
+        Publisherid: vars["publisherid"],
+    }
+    return req, nil
+}
+func decodeUpdatePublisherRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    fmt.Println("-------->>>> Into Update Decoding")
+    var req UpdatePublisherRequest
+    if err := json.NewDecoder(r.Body).Decode(&req.publisher); err != nil {
+        return nil, err
+    }
+    return req, nil
+}
+
+func encodeResponsePublisher(_ context.Context, w http.ResponseWriter, response interface{}) error {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    fmt.Println("into Encoding <<<<<<----------------")
+    return json.NewEncoder(w).Encode(response)
+}
+
+type (
+    CreatePublisherRequest struct {
+        publisher Publisher
+    }
+    CreatePublisherResponse struct {
+        Msg string `json:"msg"`
+        Err error  `json:"error,omitempty"`
+    }
+    GetPublisherByIdRequest struct {
+        Id string `json:"publisherid"`
+    }
+    GetPublisherByIdResponse struct {
+        Publisher interface{} `json:"publisher,omitempty"`
+        Err  string      `json:"error,omitempty"`
+    }
+
+    DeletePublisherRequest struct {
+        Publisherid string `json:"publisherid"`
+    }
+
+    DeletePublisherResponse struct {
+        Msg string `json:"response"`
+        Err error  `json:"error,omitempty"`
+    }
+    UpdatePublisherRequest struct {
+        publisher Publisher
+    }
+    UpdatePublisherResponse struct {
+        Msg string `json:"status,omitempty"`
+        Err error  `json:"error,omitempty"`
+    }
+)
