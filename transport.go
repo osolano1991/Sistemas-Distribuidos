d63a1269752594c7cc52ba1ccb151d50ceaea5cb
osolano1991 <oscarsolanomora11@gmail.com>
2020-09-26 18:18:26 -0600

    Update transport.go

--- transport.go
+++ transport.go
@@ -118,3 +118,117 @@ type (
         Err error  `json:"error,omitempty"`
     }
 )
+
+//=====================================================================================================================
+//                                                       Author
+//=====================================================================================================================
+
+func makeCreateAuthorEndpoint(s AuthorService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(CreateAuthorRequest)
+        msg, err := s.CreateAuthor(ctx, req.author)
+        return CreateAuthorResponse{Msg: msg, Err: err}, nil
+    }
+}
+func makeGetAuthorByIdEndpoint(s AuthorService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(GetAuthorByIdRequest)
+        authorDetails, err := s.GetAuthorById(ctx, req.Id)
+        if err != nil {
+            return GetAuthorByIdResponse{Author: authorDetails, Err: "Id not found"}, nil
+        }
+        return GetAuthorByIdResponse{Author: authorDetails, Err: ""}, nil
+    }
+}
+func makeDeleteAuthorEndpoint(s AuthorService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(DeleteAuthorRequest)
+        msg, err := s.DeleteAuthor(ctx, req.Authorid)
+        if err != nil {
+            return DeleteAuthorResponse{Msg: msg, Err: err}, nil
+        }
+        return DeleteAuthorResponse{Msg: msg, Err: nil}, nil
+    }
+}
+func makeUpdateAuthorendpoint(s AuthorService) endpoint.Endpoint {
+    return func(ctx context.Context, request interface{}) (interface{}, error) {
+        req := request.(UpdateAuthorRequest)
+        msg, err := s.UpdateAuthor(ctx, req.book)
+        return msg, err
+    }
+}
+
+func decodeCreateAuthorRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    var req CreateAuthorRequest
+    fmt.Println("-------->>>>into Decoding")
+    if err := json.NewDecoder(r.Body).Decode(&req.book); err != nil {
+        return nil, err
+    }
+    return req, nil
+}
+
+func decodeGetAuthorByIdRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    var req GetAuthorByIdRequest
+    fmt.Println("-------->>>>into GetById Decoding")
+    vars := mux.Vars(r)
+    req = GetAuthorByIdRequest{
+        Id: vars["authorid"],
+    }
+    return req, nil
+}
+func decodeDeleteBookRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    fmt.Println("-------->>>> Into Delete Decoding")
+    var req DeleteBookRequest
+    vars := mux.Vars(r)
+    req = DeleteBookRequest{
+        Bookid: vars["bookid"],
+    }
+    return req, nil
+}
+func decodeUpdateAuthorRequest(_ context.Context, r *http.Request) (interface{}, error) {
+    fmt.Println("-------->>>> Into Update Decoding")
+    var req UpdateAuthorRequest
+    if err := json.NewDecoder(r.Author).Decode(&req.author); err != nil {
+        return nil, err
+    }
+    return req, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    fmt.Println("into Encoding <<<<<<----------------")
+    return json.NewEncoder(w).Encode(response)
+}
+
+type (
+    CreateAuthorRequest struct {
+        author Author
+    }
+    CreateAuthorResponse struct {
+        Msg string `json:"msg"`
+        Err error  `json:"error,omitempty"`
+    }
+    GetAuthorByIdRequest struct {
+        Id string `json:"authorid"`
+    }
+    GetAuthorByIdResponse struct {
+        Author interface{} `json:"author,omitempty"`
+        Err  string      `json:"error,omitempty"`
+    }
+
+    DeleteAuthorRequest struct {
+        Authorid string `json:"authorid"`
+    }
+
+    DeleteAuthorResponse struct {
+        Msg string `json:"response"`
+        Err error  `json:"error,omitempty"`
+    }
+    UpdateAuthorRequest struct {
+        author Author
+    }
+    UpdateAuthorResponse struct {
+        Msg string `json:"status,omitempty"`
+        Err error  `json:"error,omitempty"`
+    }
+)
